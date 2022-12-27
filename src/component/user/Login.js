89697fee193f7183fa8cb20d2a9052d8e9bcdbb6
mdmdr8 <89672780+mdmdr8@users.noreply.github.com>
2022-12-27 19:20:03 +0900

    login
    login data

--- src/component/user/Login.js
+++ src/component/user/Login.js
@@ -6,10 +6,18 @@ import { useState } from "react";
 // import { Form, FormGroup, Label, Input, Button, Col, } from "reactstrap";
 
 function Login() {
+
+    const divStyle = {
+        width: '1200px' //캘린더 width 조절을 위해 부모태그에 설정한다.
+        , height: '600px'
+        , textAlign: 'center'
+        , margin: '100px auto'
+        , marginBottom: '20px'
+        , padding: '30px'
+        , top: '100'
+    };
+
     const [user, setUser] = useState({ id: '', password: '' });
-
-
-
     const handleInputUser = (e) => {
         console.log(e)
         const name = e.target.name;
@@ -37,36 +45,39 @@ function Login() {
     }
 
     return (
-        <Container component="main" maxWidth="xs">
-            <Box sx={{
-                marginTop: 8,
-                display: 'flex',
-                flexDirection: 'column',
-                alignItems: 'center',
-            }}>
-                {/* <Avatar sx={{ m: 1, bgcolor: 'secondary.main' }}>
-                    <LockOutlinedIcon />
-                </Avatar> */}
-                <Typography component="h1" variant="h5">
-                    Log in
-                </Typography>
+        <div style={divStyle}>
+            <Container component="main" maxWidth="xs">
+                <Box sx={{
+                    marginTop: 8,
+                    display: 'flex',
+                    flexDirection: 'column',
+                    alignItems: 'center',
+                }}>
+                    {/* <Avatar sx={{ m: 1, bgcolor: 'secondary.main' }}>
+                        <LockOutlinedIcon />
+                    </Avatar> */}
+                    <Typography component="h1" variant="h5">
+                        <div><h1><b> Log in </b></h1></div><br />
+                    </Typography>
 
-                <TextField label="ID" name='id' margin="normal" fullWidth required autoFocus onChange={handleInputUser} />
-                <TextField label="Password" type='password' name='password' margin="normal" autoComplete="current-password" fullWidth required onChange={handleInputUser} />
-                <FormControlLabel control={<Checkbox value="remember" color="primary" />} label="Remember me" />
+                    <TextField label="ID" name='id' margin="normal" fullWidth required autoFocus onChange={handleInputUser} />
+                    <TextField label="Password" type='password' name='password' margin="normal" autoComplete="current-password" fullWidth required onChange={handleInputUser} />
+                    <FormControlLabel control={<Checkbox value="remember" color="primary" />} label="Remember me" />
 
-                <Button type='submit' variant="contained" sx={{ mt: 3, mb: 2 }} fullWidth onClick={ClickLogin}>Login</Button>
+                    <Button type='submit' variant="contained" sx={{ mt: 3, mb: 2 }} fullWidth onClick={ClickLogin}>Login</Button>
 
-                <Grid container>
-                    <Grid item xs>
-                        <Link>Forgot password?</Link>
+                    <Grid container className="forget_btn">
+                        <Grid item>
+                            <Link>Forgot password?</Link>
+                        </Grid>
+                        <Grid item>
+                            <Link>Join Us</Link>
+                        </Grid>
                     </Grid>
-                    <Grid item>
-                        <Link>Join Us</Link>
-                    </Grid>
-                </Grid>
-            </Box>
-        </Container>
+
+                </Box>
+            </Container>
+        </div>
     )
 }
 
--- src/component_test/user/Login.js
+++ src/component_test/user/Login.js
@@ -1,5 +1,5 @@
 import axios from 'axios';
-import {Form, Label, Input, Button, Col, FormGroup} from 'reactstrap';
+import { Form, Label, Input, Button, Col, FormGroup } from 'reactstrap';
 
 export default function Join() {
     const divStyle = {
@@ -14,33 +14,33 @@ export default function Join() {
         , top: '100'
     };
 
-    return(
+    return (
         <div style={divStyle}>
             <div><h1><b>로그인</b></h1></div><br />
             <div style={{
-                        width: '500px'
-                        , height: '205px'
-                        , margin: '0px auto'
-                        , border: '0.5px solid gray'
-                        , padding: '30px'
-                        , borderRadius: '20px'
-                    }}>
-                <Form style={{width:"400px", margin: '0px auto'}}>
+                width: '500px'
+                , height: '205px'
+                , margin: '0px auto'
+                , border: '0.5px solid gray'
+                , padding: '30px'
+                , borderRadius: '20px'
+            }}>
+                <Form style={{ width: "400px", margin: '0px auto' }}>
                     <FormGroup row>
-                            <Label htmlFor='id' sm={4}>아이디</Label>
-                            <Col sm={8}>
-                                <Input type='text' name='id' id='id' />
-                            </Col>
+                        <Label htmlFor='id' sm={4}>아이디</Label>
+                        <Col sm={8}>
+                            <Input type='text' name='id' id='id' />
+                        </Col>
                     </FormGroup>
                     <FormGroup row>
-                            <Label htmlFor='password' sm={4}>비밀번호</Label>
-                            <Col sm={8}>
-                                <Input type='text' name='password' id='password' />
-                            </Col>
+                        <Label htmlFor='password' sm={4}>비밀번호</Label>
+                        <Col sm={8}>
+                            <Input type='text' name='password' id='password' />
+                        </Col>
                     </FormGroup>
                     <FormGroup row>
-                         <Col sm={4} >
-                            <Button color='secondary' style={{width:'400px'}} >로그인</Button>
+                        <Col sm={4} >
+                            <Button color='secondary' style={{ width: '400px' }} >로그인</Button>
                         </Col>
                     </FormGroup>
                 </Form>
