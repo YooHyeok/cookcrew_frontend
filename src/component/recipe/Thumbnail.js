60ef0f252cab61a4b1dc759d11318a30b0705a00
JoHB94 <whgusqls113@gmail.com>
2022-12-28 20:33:00 +0900

    conflict

--- src/component/recipe/Thumbnail.js
+++ src/component/recipe/Thumbnail.js
@@ -95,10 +95,10 @@ export default function Tumbnail() {
                 <Fragment>
                     <Container fixed id='detail_container'>
                         <div id='icons_group'>
-                            {/* 조회수, 별점, 찜갯수 들어가는 곳 */}
+                            {/* 조회수, 별점, 찜갯수 들어가는 곳 */}             
                             <span><BsFillEyeFill />&nbsp;{cnt}</span>&nbsp;&nbsp;
                             <span><BsFillStarFill />&nbsp;{ratingValue}</span>&nbsp;&nbsp;
-                            <span><BsHeartFill />&nbsp;17</span>&nbsp;&nbsp;
+                            <span><BsHeartFill />&nbsp;17</span>&nbsp;&nbsp;                           
                         </div><hr />
                         <div id='explain_recipe'>
                             <div id='exp_title'>
