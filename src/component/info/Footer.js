c15d361f2cde87484b89c2dba5c1e345aafea5c9
92Lee92 <devindeed92@gmail.com>
2022-12-28 21:36:48 +0900

    Header Footer css 수정

--- src/component/info/Footer.js
+++ src/component/info/Footer.js
@@ -1,17 +1,19 @@
+import { margin, textAlign } from '@mui/system';
 import { Link } from 'react-router-dom';
 
 export default function Footer() {
     const style = {
         height:"160px",
         marginTop:"auto",
-        backgroundColor:"#dde0ea"
+        backgroundColor:"#dde0ea",
+
     }
     return(
-    <div style={style}>
+    <div className='bg-slate-200'>
             <ul style = {{height:"140px"}} className="footer-items " >
-                <li className="footer-item">
+                <li className="footer-item inline my-4 mx-4">
                     {/* 로고 영역 */}
-                    <Link to={'/'} id="logo">Cook<b>Crew</b><img width={30} src={require('../../resources/img/A_test.png')} alt=''/></Link>
+                    <Link to={'/'} id="logo"><img className='inline w-32' src={require('../../resources/img/cookcrew.png')} alt=''/></Link>
                 </li>
                 <li className="footer-item">
                     <Link to={'/myrecipe'}>나의 레시피</Link>
