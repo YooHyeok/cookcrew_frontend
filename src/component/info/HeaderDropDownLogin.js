0f6a8d1b8ecc58df0a785d68910a7ae6459ed30c
92Lee92 <devindeed92@gmail.com>
2022-12-29 17:39:14 +0900

    merge commit

--- src/component/info/HeaderDropDownLogin.js
+++ src/component/info/HeaderDropDownLogin.js
@@ -8,7 +8,6 @@ export default function HeaderDropDownLogin() {
 
   const context = useContext(HeaderDropDownContext);
 
-<<<<<<< HEAD
     return (
       <Dropdown id="ok" isOpen={context.dropdownOpenLogin} fade="true" toggle={context.toggleLogin}>
         <DropdownToggle caret style={{backgroundColor:"rgb(0,0,0,0)", border:"none"}}>
@@ -23,20 +22,4 @@ export default function HeaderDropDownLogin() {
         </DropdownMenu>
       </Dropdown>
     );
-=======
-  return (
-    <Dropdown id="ok" isOpen={context.dropdownOpenLogin} fade="true" toggle={context.toggleLogin}>
-      <DropdownToggle caret style={{ backgroundColor: "rgb(0,0,0,0)", border: "none" }}>
-        <PersonCircle size={30} />
-      </DropdownToggle>
-      <DropdownMenu >
-        {/* <DropdownItem style={{lineHeight: "25px"}} header>header</DropdownItem>
-          <DropdownItem style={{lineHeight: "25px"}} disabled>Action</DropdownItem> */}
-        <Link to={'/login'}><DropdownItem style={{ lineHeight: "25px" }}><b>로그인</b></DropdownItem></Link>
-        <DropdownItem style={{ lineHeight: "25px" }} divider />
-        <Link to={'/join'}><DropdownItem style={{ lineHeight: "25px" }}><b>회원가입</b></DropdownItem></Link>
-      </DropdownMenu>
-    </Dropdown>
-  );
->>>>>>> zeze
 }
